ebad536d1a76ad8572ef5b71c4a2588be3f81cef
srinivas321 <srinivas@sun-rack.com>
2025-04-24 16:55:15 +0530

    change the color theme

--- lib/theme/colors.dart
+++ lib/theme/colors.dart
@@ -1,19 +1,19 @@
 import 'package:flutter/material.dart';
 
 class AppColors {
-  static const background = Color(0xFF0F172A);
-  static const cardBackground = Color(0xFF1E2746);
+  static const background = Color(0xFF0A0F1C);
+  static const cardBackground = Color(0xFF131B2E);
   static const accentCyan = Color(0xFF7DF9FF);
-  static const textGrey = Color(0xFF64748B);
+  static const textGrey = Color(0xFF798598);
   static const white = Colors.white;
-  static const borderColor = Color(0xFF334155);
-  static const inputBackground = Color(0xFF1E293B);
-  
+  static const borderColor = Color(0xFF2A3F5B);
+  static const inputBackground = Color(0xFF0D1526);
+
   // New colors for task status
   static const pending = Color(0xFFFF4444);
   static const inProgress = Color(0xFF3B82F6);
   static const completed = Color(0xFF22C55E);
-  
+
   // Priority colors
   static const highPriority = Color(0xFFFF4444);
   static const mediumPriority = Color(0xFFFFB800);
